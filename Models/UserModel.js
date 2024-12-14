b4451fb82dfb0d757546cf084f053651caf42c58
Yash Saxena <yashsaxena@Yashs-MacBook-Air.local>
2024-12-15 04:38:12 +0530

    Bcrypt Updated

--- Models/UserModel.js
+++ Models/UserModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const { token } = require('morgan')
 const { required } = require('@hapi/joi')
 const UserSchema = new Schema({
